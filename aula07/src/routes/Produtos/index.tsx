6336902afab291a435494f144d666e4ff98e83b5
victoraugustogfavaro <victoraugustogfavaro@gmail.com>
2024-08-29 11:28:26 -0300

    Gerando dados de produtos através de lista externa

--- aula07/src/routes/Produtos/index.tsx
+++ aula07/src/routes/Produtos/index.tsx
@@ -1,13 +1,48 @@
+import { Link } from "react-router-dom";
+import { listaProdutos } from "../../listaProdutos";
 
+export default function Produtos() {
+  //MUDANDO O TÍTULO DA PÁGINA!!!
+  document.title = "PRODUTOS";
 
-export default function Produtos(){
+  return (
+    <div>
+      <h1>Olá, mundo sou o Produtos!</h1>
+      <table>
+        <thead>
+          <th>Id</th>
+          <th>Nome</th>
+          <th>Preço</th>
+          <th>Descrição</th>
+          <th>Imagem</th>
+          <th>Editar</th>
+        </thead>
 
-      //MUDANDO O TÍTULO DA PÁGINA!!!
-      document.title = "PRODUTOS";
+        <tbody>
+          {listaProdutos.map((produto) => (
+            <tr key={produto.id}>
+              <td> {produto.id}</td>
+              <td> {produto.nome}</td>
+              <td> {produto.preco}</td>
+              <td> {produto.desc}</td>
+              <td>
+                <img src={produto.imagem} alt={produto.nome} />
+              </td>
+              <td>
+                <Link to={`/editar/produtos/${produto.id}`}>Editar</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
 
-    return(
-      <div>
-        <h1>Olá, mundo sou o Produtos!</h1>
-      </div>
-    );
-  }
+        <tfoot>
+          <tr>
+            <td colSpan={5}>
+              Total de produtos: <span>{listaProdutos.length}</span>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
+}
