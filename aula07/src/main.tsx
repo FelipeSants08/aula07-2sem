cdc931dfbeec12e61bfa456e5c271f6da59d7d3a
jenniesuzuki <jenniferkaorisuzuki@gmail.com>
2024-08-29 11:01:03 -0300

    Params

--- aula07/src/main.tsx
+++ aula07/src/main.tsx
@@ -14,7 +14,7 @@ const routes = createBrowserRouter([
   {path:"/",element:<App/>,errorElement:<Error/>,children:[
     {path:"/",element:<Home/>},
     {path:"/produtos",element:<Produtos/>},
-    {path:"/editar/produtos/:id",element:<EditarProdutos/>},
+    {path:"/editar/produtos/:id/:id2",element:<EditarProdutos/>},
   ]} 
 ])
 
