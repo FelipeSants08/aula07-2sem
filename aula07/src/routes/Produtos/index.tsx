232e071c9f94e018e1a2836b43c9e285b6a6559c
MarceloScoleso <marcelo.scolesojr@gmail.com>
2024-08-29 11:28:32 -0300

    Gerando dados de produtos através de listas externas

--- aula07/src/routes/Produtos/index.tsx
+++ aula07/src/routes/Produtos/index.tsx
@@ -1,4 +1,5 @@
-
+import { Link } from "react-router-dom";
+import { listaProdutos } from "../../ListaProdutos";
 
 export default function Produtos(){
 
@@ -7,7 +8,36 @@ export default function Produtos(){
 
     return(
       <div>
-        <h1>Olá, mundo sou o Produtos!</h1>
+        <h1>Lista de Produtos</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Nome</th>
+              <th>Preço</th>
+              <th>Descrição</th>
+              <th>Imagem</th>
+              <th>Editar</th>
+            </tr>
+          </thead>
+          <tbody>
+              {listaProdutos.map((produto) => (
+                <tr key={produto.id}>
+                  <td>{produto.id}</td>
+                  <td>{produto.nome}</td>
+                  <td>{produto.preco}</td>
+                  <td>{produto.desc}</td>
+                  <td><img src={produto.imagem} alt= {produto.nome}/></td>
+                  <td><Link to={'/editar/produtos/${produto.id}'}> Editar</Link></td>
+                </tr>
+              ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={5}>Total de produtos: <span> {listaProdutos.length}</span></td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
     );
   }
