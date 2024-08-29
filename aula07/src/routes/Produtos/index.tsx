1c1c2e98a4144ed5dbaefd8eb279b90791099ec2
Alexandre Carlos <alefiap@hotmail.com>
2024-08-29 10:15:24 -0300

    Boyler plate pronto....

---
+++ aula07/src/routes/Produtos/index.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Produtos(){
+
+      //MUDANDO O TÍTULO DA PÁGINA!!!
+      document.title = "PRODUTOS";
+
+    return(
+      <div>
+        <h1>Olá, mundo sou o Produtos!</h1>
+      </div>
+    );
+  }
