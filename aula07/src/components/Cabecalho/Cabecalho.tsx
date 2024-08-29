f4d0108c8305417871d9ceab4fb26a82654cd1c2
vitin46 <viegidulira@gmail.com>
2024-08-29 12:08:53 -0300

    coisa pra dedeu nao me lembro

--- aula07/src/components/Cabecalho/Cabecalho.tsx
+++ aula07/src/components/Cabecalho/Cabecalho.tsx
@@ -1,10 +1,20 @@
 import { Header } from "../../style/styled";
 import Menu from "../Menu/Menu";
+import { Lista } from "../../types";
+import { listaProdutos } from "../../listaProdutos";
 
-export default function Cabecalho(){
+export default function Cabecalho({listaProps}:{listaProps:Lista[]}){
+
   return(
     <Header>
       <h1>Cabeçalho</h1>
+
+        <div>
+          {listaProps.map((produto)=>(
+            <p key={produto.id}>{produto.nome}</p>
+          ))}
+        </div>
+
       <Menu/>
     </Header>
   );
--- aula07/src/listaProdutos.ts
+++ aula07/src/listaProdutos.ts
@@ -1,48 +1,43 @@
+import { Lista } from "./types";
 
-//Criar uma lista rde produtos eletronicos com os seguintes dados:
-// id,nome,precom,desc,imagem=path
-type Lista = {
-    id: number,
-    nome: string,
-    preco: number,
-    desc: string,   
-    imagem: string
-}
+//Criar uma lista de produtos eletrônicos com os seguintes dados:
+//id,nome,preco,desc,imagem=path
+
 
 export const listaProdutos:Lista[] = [
     {
         id: 1,
-        nome: 'Smartphone',
+        nome: "Smartphone",
         preco: 1500.00,
-        desc: 'Celular com tela de 6 polegadas',
+        desc: "Celular com tela de 6 polegadas",
         imagem: "https://example.com/imagem1.jpg"
     },
     {
         id: 2,
-        nome: 'Tablet',
+        nome: "Tablet",
         preco: 800.00,
-        desc: 'Tablet com tela de 10 polegadas',
+        desc: "Tablet com tela de 10 polegadas",
         imagem: "https://example.com/imagem2.jpg"
     },
     {
         id: 3,
-        nome: 'Notebook',
+        nome: "Notebook",
         preco: 2500.00,
-        desc: 'Notebook com processador Intel Core i7',
+        desc: "Notebook com processador Intel Core i5",
         imagem: "https://example.com/imagem3.jpg"
     },
     {
         id: 4,
-        nome: 'Impressora',
+        nome: "Impressora",
         preco: 300.00,
-        desc: 'Impressora laser com qualidade de impressão alta',
+        desc: "Impressora de alta qualidade",
         imagem: "https://example.com/imagem4.jpg"
     },
     {
         id: 5,
-        nome: 'Fone de Ouvido',
+        nome: "Fone de Ouvido",
         preco: 200.00,
-        desc: 'Fone de ouvido sem fio com qualidade de som alta',
+        desc: "Fone de ouvido sem fio",
         imagem: "https://example.com/imagem5.jpg"
     }
-]
+];
