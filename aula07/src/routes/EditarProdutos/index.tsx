dfff13e159a136d822d8971b7128ca7e9fd07337
jenniesuzuki <jenniferkaorisuzuki@gmail.com>
2024-08-29 11:10:29 -0300

    Parametrizando e utilizando o hook useParams

--- aula07/src/routes/EditarProdutos/index.tsx
+++ aula07/src/routes/EditarProdutos/index.tsx
@@ -10,17 +10,13 @@ export default function EditarProdutos(){
       //O destructuring irá recuperar o dado que foi passado na rota e atribuir a propriedade de um elemento cuja o nome é aquele criado na rota, antes do dois pontos,ou seja, (:dados)
       //Então teriamos que realizar a seguinte ação para receber esta informação.
       // const{dados} = useParams(), um detalhe aqui é que o useParams() pertence ao react-router e deve ser importado dele
-      const {id, id2} = useParams();
-
-      const novoId1 = useParams().id;
-      const novoId2 = useParams().id2;
+      const {id} = useParams();
 
       return(
       <div>
         <h1>Olá, mundo sou o EditarProdutos!</h1>
         <div>
           <h2>ID: {id}</h2>
-          <h2>ID2: {novoId2}</h2>
         </div>
       </div>
     );
